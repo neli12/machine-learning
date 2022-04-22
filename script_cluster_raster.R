5e63552e0de456d88833992cf05a8b7825510daa
Neli <neliquinhonez92@gmail.com>
2022-04-22 11:27:02 -0300

    Update script_cluster_raster.R

--- script_cluster_raster.R
+++ script_cluster_raster.R
@@ -16,12 +16,12 @@ for (i in 1:length(images)){
 covs.stack <- stack(covs)
 
 # Get the values of the raster stack
-rvals <- getValues(covs.stack)
-summary(rvals)
+stack_vals <- getValues(covs.stack)
+summary(stack_vals)
 
 # Elbow method to check the optimal number of cluster
 k.max <- 20
-data <- (na.omit(scale(rvals))) #scale and exclude NA's
+data <- (na.omit(scale(stack_vals))) #scale and exclude NA's
 wss <- sapply(1:k.max, 
               function(k){kmeans(data, k, nstart=50,iter.max = 15 )$tot.withinss})
 wss
@@ -37,20 +37,20 @@ summary(clus)
 
 
 # Create am index
-idx <- 1:ncell(covar)
-idx <- idx[-unique(which(is.na(rvals), arr.ind=TRUE)[,1])] 
+idx <- 1:ncell(covs.stack)
+idx <- idx[-unique(which(is.na(stack_vals), arr.ind=TRUE)[,1])] 
 str(idx)
 
 # Create an empty raster using the first raster of your stack
-r.clust <- covs.stack[[1]]
-plot(r.clust)
-r.clust[] <- NA
+clust_raster <- covs.stack[[1]]
+plot(clust_raster)
+clust_raster[] <- NA
 
 # Transfer the clustering results to your empty raster and save it
-r.clust[idx] <- clus$clustering
-plot(r.clust) 
-crs(r.clust)
+clust_raster[idx] <- clus$clustering
+plot(clust_raster) 
+crs(clust_raster)
 
-writeRaster(r.clust, "Clusters", format = "GTiff", datatype = "FLT4S", overwrite = T)
+writeRaster(clust_raster, "Clusters", format = "GTiff", datatype = "FLT4S", overwrite = T)
 
 
